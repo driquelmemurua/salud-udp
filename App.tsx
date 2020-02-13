874b00ad9dab742f1c2bc35311998eaeefa307dc
Diego Riquelme Murúa <diego.riquelme@koffguerrero.com>
2020-02-13 18:46:25 -0300

    Desarrollo

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Menu, Contactos, Direcciones, Escuela, Instructivos, Sugerencias } from './views';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <Contactos/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
