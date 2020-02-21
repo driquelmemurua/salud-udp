15db055af42f5ccc00a4271c213dd33d5f1b031c
Diego Riquelme Murúa <diego.riquelme@koffguerrero.com>
2020-02-21 18:47:29 -0300

    instalado react-navigation

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Menu, Contactos, Direcciones, Escuela, Instructivos, Sugerencias } from './views';
 
+const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
-    <Menu/>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen
+          name={'Menu'}
+          component={Menu}
+        />
+        <Stack.Screen
+          name={'Cambiar Escuela'}
+          component={Escuela}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
