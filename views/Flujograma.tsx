246ecad23eca30d15c07349e79730b3d88637134
Sebastian <sebastian.mofre@gmail.com>
2020-02-24 21:33:45 -0300

    webview pdf viewer online

--- views/Flujograma.tsx
+++ views/Flujograma.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { Default } from '../layouts';
+import * as FileSystem from 'expo-file-system';
+
+
+import {WebView} from 'react-native-webview';
+
 
 function Flujograma({ route, navigation }) {
  
   const { name } = route.params;
-
+  
   return (
-  <Default
-    title={name}
-    navigation={navigation.goBack}>
-
-  </Default>
+    <WebView
+      source={{uri: 'https://drive.google.com/viewerng/viewer?embedded=true&url=http://gahp.net/wp-content/uploads/2017/09/sample.pdf'}}
+      style={{marginTop: 20}}
+      />
 );
 }
 
