b64c77f91f04e66f55f57718fec136386d3d0237
Diego Riquelme Murúa <diego.riquelme@koffguerrero.com>
2020-03-07 20:57:24 -0300

    Se guarda escuela en storage

--- views/Menu.tsx
+++ views/Menu.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import { FlatList, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList, Text, TouchableOpacity, AsyncStorage } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import { Default } from '../layouts';
 import { styles } from '../styles';
 import { CONTENT_VIEWS } from '../constants';
-import { connect } from 'react-redux';
+import { allActions } from '../actions';
 
-function Menu({navigation, selectedSchool}) {
+function Menu({navigation, selectedSchool, updateSchool}) {
+
+  const [school, setSchool] = useState(selectedSchool);
+
+  useEffect(() => {
+    if(!school){
+      _retrieveSchool()
+      .then((value) => {
+        if(value){
+          updateSchool(value);
+          setSchool(value);
+        } else {
+          navigation.navigate('Cambiar Escuela');
+        }
+      })
+    }
+  },[school])
+
+  const _retrieveSchool = async () => {
+    try {
+      console.log('RETRIEVE SCHOOL');
+      const value = await AsyncStorage.getItem('selectedSchool');
+      console.log(value)
+      return value;
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
   return (
     <Default
       logo
@@ -29,4 +59,11 @@ function mapStateToProps(state) {
   return { selectedSchool: state.schools.selectedSchool };
 } 
 
-export default connect(mapStateToProps)(Menu);
+const mapDispatchToProps = dispatch => {
+  const { updateSchool } = allActions.schoolActions;
+  return bindActionCreators({
+    updateSchool,
+  }, dispatch)
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Menu);
