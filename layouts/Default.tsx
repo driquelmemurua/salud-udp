8b4f1604157833623ac64d0a07452b85ec8bf696
Diego Riquelme Murúa <diego.riquelme@koffguerrero.com>
2020-03-07 22:13:14 -0300

    Bugfix goBack

--- layouts/Default.tsx
+++ layouts/Default.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, Image, TouchableWithoutFeedback } from 'react-native';
 import { styles } from '../styles';
 import { Ionicons } from '@expo/vector-icons';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
 function Default({children, title = null, subtitle = null, logo = false, navigation = null}) {
+
+  const [isGoingBack, setIsGoingBack] = useState(false);
+  
+  useEffect(() => {
+    if(isGoingBack){
+      navigation()
+    }
+  },[isGoingBack])
+
   return (
     <View style={styles.container}>
       <View style={{flexDirection: 'row', width: '80%', justifyContent: 'center'}}>
         {navigation ?
-          <TouchableWithoutFeedback onPress={() => navigation()}>
+          <TouchableWithoutFeedback onPress={() => setIsGoingBack(true)}>
             <View hitSlop={{top: 20, bottom: 20, left: 20, right: 20}}>
               <Ionicons name="md-arrow-back" size={24} color="white" />
             </View>
